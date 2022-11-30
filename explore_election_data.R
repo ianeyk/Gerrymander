a62f6a00df2928fdb2305b682243b38964d6958c
Ian Eykamp <ian@eykamp.com>
2022-11-29 19:13:15 -0500

    small changes

--- explore_election_data.R
+++ explore_election_data.R
@@ -1,7 +1,6 @@
 load("election_data.Rdata")
 
+# show that for a given VTD, there are multiple VTDKEY's in different counties
 election %>% 
-  distinct(VTD)
+  filter(VTD == "0015")
 
-election %>% 
-  filter(VTD == 0015)
