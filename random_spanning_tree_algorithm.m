c5a6b0463ee43fc968923f6b151bdd17b062e817
Ian Eykamp <ian@eykamp.com>
2022-12-05 10:34:41 -0500

    almost working spanning tree algorithm

--- random_spanning_tree_algorithm.m
+++ random_spanning_tree_algorithm.m
@@ -13,29 +13,46 @@ NodeTable = table(nodes, 'VariableNames',{'VTDKEY'});
 NodeTable = join(NodeTable,population);
 
 G = graph(EdgeTable, NodeTable);
-plot(G)
+% plot(G)
 %% Spanning Tree Algorithm
 G2 = G;
 
 root = randsample(nodes, 1);
 
-% T = graph; % final spanning tree
-S = graph;
+T = graph; % final spanning tree
 u = root;
 
 for node = 1:length(nodes)
     
+    S = graph; % intermediate spanning tree
     u = node;
     
+    stop_while = false;
     at_least_one_iteration = false;
-    while at_least_one_iteration | ~(any(S.Edges.EndNodes(1:end-1) == u))
-        next_node = randsample(find(A(u, :)), 1);
-        S = addedge(S, u, next_node);
-        u = next_node;
-
-        at_least_one_iteration = false;
+    while ~stop_while
+        stop_while = any(T.Edges.EndNodes(1:end-1) == u) | length(S.Edges.EndNodes) > 1000;
+        if (~stop_while | true)
+            next_node = randsample(find(A(u, :)), 1);
+            
+            alpha = S.Edges.EndNodes(:, 1) == next_node;
+            if any(alpha)
+                S = rmedge(S, find(alpha));
+    %             S.Edges.EndNodes(alpha, :) = [u, next_node]; % redefine the edge
+            end
+            S = addedge(S, u, next_node); % add new edge
+            
+            u = next_node;
+    
+            at_least_one_iteration = false;
+        end
+%         n_nodes = size(S.Edges.EndNodes)
+%         stop_while = (at_least_one_iteration | ~(any(T.Edges.EndNodes(1:end-1) == u)) & length(S.Edges.EndNodes) < 1000);
+%         stop_while = any(T.Edges.EndNodes(1:end-1) == u) | length(S.Edges.EndNodes) > 1000;
     end
 
+    T = addedge(T, S.Edges);
+
+    n_nodes_in_T = size(T.Edges.EndNodes)
 %     S = addedge(S, u, next_node);
 
 %     if any(S.nodes == next_node)
@@ -44,10 +61,6 @@ for node = 1:length(nodes)
 
 end
 
-result = S.Edges;
-
 %% plot
-S
-plot(S);
-%% Part 2
-bins = conncomp(S)
+T
+plot(T);
