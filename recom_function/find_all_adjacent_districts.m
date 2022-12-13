4f1c2b21e74887833024fbd4519a4a526baa3116
Ian Eykamp <ian@eykamp.com>
2022-12-13 13:14:45 -0500

    greatly simplify and speed up find_all_adjacent_districts.m

--- recom_function/find_all_adjacent_districts.m
+++ recom_function/find_all_adjacent_districts.m
@@ -1,54 +1,13 @@
-function D = find_all_adjacent_districts(G, old_G, old_D, districts_to_find, skip_delete)
-    A = adjacency(G);
-    nodes = unique(G.Edges.EndNodes);
+function D = find_all_adjacent_districts(G_iter)
+    D = graph();
+    
+    % For each edge, get the district_id's of the end nodes, then take only the unique (distinct) pairs of districts
+    % which are connected to each other.
+    district_connections = unique(G_iter.Nodes.district_id(G_iter.Edges.EndNodes), 'rows');
 
-    n_districts = max(G.Nodes.district_id);
-
-%     if skip_delete
-%         D = graph();
-%         for ii = 1:length(districts_to_find)
-%             district_id = districts_to_find(ii);
-%             D = addnode(D, table(district_id))
-%         end
-%     else
-        D = old_D;
-%     end
-
-
-    % start by removing all the bad edges from old_D
-    old_A = adjacency(old_G);
-    for kk = 1:length(districts_to_find)
-        district_1 = districts_to_find(kk);
-        district_1_nodes = find(G.Nodes.district_id == district_1);
-
-        % remove old edges
-        if ~skip_delete
-        for ii = 1:length(district_1_nodes)
-            district_1_node = district_1_nodes(ii);
-            adjacent_nodes = find(old_A(district_1_node, :)); % old adjacency matrix
-            for jj = 1:length(adjacent_nodes)
-                adjacent_node = adjacent_nodes(jj);
-                adjacent_district = G.Nodes.district_id(adjacent_node);
-                D = rmedge(D, district_1, adjacent_district);
-            end
-        end
-        end
-
-        % add new edges
-        for ii = 1:length(district_1_nodes)
-            district_1_node = district_1_nodes(ii);
-            adjacent_nodes = find(A(district_1_node, :)); % new adjacency matrix
-            for jj = 1:length(adjacent_nodes)
-                adjacent_node = adjacent_nodes(jj);
-                adjacent_district = G.Nodes.district_id(adjacent_node);
-                D = addedge(D, district_1, adjacent_district);
-            end
-        end
+    % Add the distinct edges to the graph of adacent district
+    for dd = 1:size(district_connections, 1)
+        D = addedge(D, district_connections(dd, 1), district_connections(dd, 2));
     end
-
-    D = simplify(D);
-    if ~(size(D.Nodes, 1) == n_districts)
-        disp("Number of nodes does not line up.")
-        n_districts
-    end
+    D = simplify(D); % remove self loops
 end
--- recom_function/recomb.m
+++ recom_function/recomb.m
@@ -3,49 +3,17 @@ clear;
 close;
 load("recomb_initial_state.mat");
 
-
+% G_iter is the graph that will hold the new district ID's. G is preserved as a reference, with the original district ID's.
 G_iter = G;
-% D_old = graph();
-% D = find_all_adjacent_districts(G, G_iter, D_old, 1:max(G.Nodes.district_id), true);
 
-
-% n_districts_to_combine = 900;
-% D_nodes = unique(D.Edges.EndNodes);
-% districts_to_combine = D_nodes(randsample(1:length(D_nodes), n_districts_to_combine, true));
-% while length(unique(G_iter.Nodes.district_id)) > 5
-%     district1 = D.Edges.EndNodes(randsample(1:length(D.Edges.EndNodes(:, 1)), 1));
-
-%     AD = adjacency(D);
-%     adjacent_nodes = find(AD(district1, :)); % get a list of nodes adjacent to district1 from the adjacency matrix
-%     % This if statement is needed because randsample returns a different value if the input has length 1 vs. length > 1
-%     if length(adjacent_nodes) == 1
-%         district2 = adjacent_nodes;
-%     else
-%         district2 = randsample(adjacent_nodes, 1);
-%     end
-
-%     %     district1 = districts_to_combine(dd, 1);
-%     %     district2 = districts_to_combine(dd, 2);
-%     G_iter.Nodes.district_id(G_iter.Nodes.district_id == district2) = district1;
-%     len_unique = length(unique(G_iter.Nodes.district_id))
-% end
-% %%
-% unique_district_ids = unique(G_iter.Nodes.district_id)
-% unique_districts = zeros(length(G_iter.Nodes.district_id), 1);
-% for uu = 1:length(unique_district_ids)
-%     unique_district_id = unique_district_ids(uu);
-%     unique_districts(G_iter.Nodes.district_id == unique_district_id) = uu;
-% end
-% % G_iter.Nodes.district_id = unique_districts;
-
+% initialize all nodes to district_id = 1. Then pick 5 nodes to re-assign.This creates 1 massive district and 4 tiny ones
 G_iter.Nodes.district_id = zeros(height(G.Nodes), 1) + 1;
 node_ids_to_change = randsample(1:height(G.Nodes), 5);
 for gg = 1:5
     G_iter.Nodes.district_id(node_ids_to_change(gg)) = gg;
 end
 
-
-D = find_all_adjacent_districts(G_iter, G_iter, graph(), unique(G_iter.Nodes.district_id), true);
+D = find_all_adjacent_districts(G_iter);
 %%
 unique_districts = G_iter.Nodes.district_id;
 plt = plot(G_iter, 'XData', centroids(:,1), 'YData', centroids(:,2));
